6d0e551ce45896c35e87621cfdb11c34db797b92
Sohan Sharma <sohangamedev@gmail.com>
2025-02-02 17:37:09 +0530

    Point Cloud

--- Assets/Scripts/GPU/graph_gpu.cs
+++ Assets/Scripts/GPU/graph_gpu.cs
@@ -3,55 +3,64 @@ using TMPro;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class GPUGraph : MonoBehaviour
 {
-    const int maxResolution = 5000;
-
+    const int maxResolution = 7071;
 
     [SerializeField] TextMeshProUGUI count;
     [SerializeField] private Slider slider;
 
-    [SerializeField]
-    ComputeShader shader;
-
     [SerializeField] Material material;
     [SerializeField] Mesh mesh;
 
+    [SerializeField] ComputeShader shader;
+    
+    // it uses as range between max resolution
     private int resolution;
 
+    private ComputeBuffer buffer_position;
 
+    // Kernel -> links to the compute shader 
+    private int kernel; 
+    private float step;
+
+    // Store values for the compute shader : Uniforms IN
     static readonly int
         positionsId = Shader.PropertyToID("_positions"),
         resId = Shader.PropertyToID("_resolution"),
         stepId = Shader.PropertyToID("_step"),
         timeId = Shader.PropertyToID("_time");
 
-    // Compute Shader Variables
-    private ComputeBuffer buffer_position;
-    private int kernel;
-    private float3[] positions;
-
-    private float step;
-
     private void Awake()
     {
-
-        // Set up slider limits
         slider.minValue = 10;
         slider.maxValue = maxResolution;
         slider.wholeNumbers = true;
 
         slider.onValueChanged.AddListener(OnSliderChanged);
-        resolution = (int)slider.value; // Set resolution to initial slider value
+        resolution = (int)slider.value;
 
         kernel = shader.FindKernel("CSMain");
+
+
+        // Instantiate -> single vertex if none are given.
+        if(mesh == null)
+        {
+            mesh = new Mesh();
+            int[] indices = { 0 };
+
+            mesh.vertices = new[] { Vector3.zero };
+            mesh.SetIndices(indices, MeshTopology.Points, 0);
+        }
     }
 
-    // Max Resolution -> creates buffer positions for maximum number of enteries;
     void OnEnable()
     {
-        buffer_position = new ComputeBuffer(maxResolution * maxResolution, sizeof(float) * 3);  // stride -> position has 3 floats
+        // buffer -> allocates the positions into this buffer for maximum resolution
+        buffer_position = new ComputeBuffer(maxResolution * maxResolution, sizeof(float) * 3);
     }
+
     void Update()
     {
         UpdateFunction();
@@ -61,6 +70,8 @@ public class GPUGraph : MonoBehaviour
     {
         step = 2f / (resolution - 1);
 
+
+        // Deport values to the compute shader : Set Uniforms
         shader.SetInt(resId, resolution);
         shader.SetFloat(stepId, step);
         shader.SetFloat(timeId, Time.time);
@@ -70,22 +81,25 @@ public class GPUGraph : MonoBehaviour
         int groups = Mathf.CeilToInt((float)resolution / 8.0f);
         shader.Dispatch(kernel, groups, groups, 1);
 
+
+        // It is a frustum culling
         var bounds = new Bounds(Vector3.zero, Vector3.one * (2f + 2f / resolution));
 
         material.SetBuffer(positionsId, buffer_position);
         material.SetFloat(stepId, step);
 
+        // Render as single points
         Graphics.DrawMeshInstancedProcedural(mesh, 0, material, bounds, resolution * resolution);
     }
 
     public void OnSliderChanged(float value)
     {
-        resolution = (int)value; // Update resolution from slider
+        resolution = (int)value;
         int total = resolution * resolution;
         count.text = total.ToString("N0");
     }
 
-
+    // clean ups
     private void OnDisable()
     {
         buffer_position.Release();
